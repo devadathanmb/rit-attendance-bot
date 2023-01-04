f26f5f17568cf34c7882655e95bcef3bd6b3b44f
Devadathan M B <devadathanmb@gmail.com>
2023-01-04 14:00:38 +0530

    initial : Handle commands /login and /attendance

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ bot.use(new LocalSession({ database: 'session_log.json' }).middleware())
 bot.command('help', (ctx) => ctx.reply('I will help you'))
 
 bot.command('login', async (ctx) => {
-  console.log('yes sir')
+  console.log('/login')
   const credentials = ctx.message.text.split(' ')[1]
   const username = credentials.split(':')[0]
   const password = credentials.split(':')[1]
@@ -29,23 +29,36 @@ bot.command('login', async (ctx) => {
     const response = await axios.post(`${API_URL}/login`, requestBody)
     const cookie = response.data['session-cookie']
     ctx.session.session_cookie = cookie
-    console.log(ctx.message.chat.id)
-    console.log(cookie)
+    ctx.reply(
+      'You are logged in. Now type /attendance to get attendance details.'
+    )
   } catch (error) {
-    if ((error.response.status = 401)) {
+    if (error.response.status == 401) {
       ctx.reply('Invalid username or password sir')
+    } else if (error.response.status == 440) {
+      ctx.reply('Session expired. Please login again.')
     }
   }
 })
 
 bot.command('attendance', async (ctx) => {
+  console.log("/attendance")
   const cookie = ctx.session.session_cookie
   const response = await axios.get(`${API_URL}/attendance`, {
     headers: {
       Cookie: `session_cookie=${cookie};`,
     },
   })
-  ctx.reply(response.data.name)
+  response.data.subject_attendance.forEach(subject => {
+    `${subject.subject_name} (${subject.subject_code}): ${subject.present_hours}/${subject.total_hours} ${subject.present_hours}`
+  });
+  const message = `
+Name : ${response.data.name}  
+Admission no : ${response.data.admission_no}
+Total attendance : ${response.data.total_attendance}
+`
+  message.replace('.', '\\.')
+  ctx.reply(message)
 })
 
 // Start webhook via launch method (preferred)
