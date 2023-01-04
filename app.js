62db12253e78d2244b9a14b50f2cab7204d2390e
Devadathan M B <devadathanmb@gmail.com>
2023-01-04 21:30:57 +0530

    feat : Add code for commands - help, attendance, start, other

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const { Telegraf } = require('telegraf')
 const axios = require('axios')
 const LocalSession = require('telegraf-session-local')
+const Quote = require('inspirational-quotes')
 
 const API_TOKEN = process.env.API_TOKEN
 const SERVER_URL = process.env.SERVER_URL
@@ -13,61 +14,173 @@ axios.defaults.withCredentials = true
 const bot = new Telegraf(API_TOKEN)
 bot.use(new LocalSession({ database: 'session_log.json' }).middleware())
 
-bot.command('help', (ctx) => ctx.reply('I will help you'))
+// Start
+bot.start((ctx) => {
+  const message = `Hello ${ctx.chat.username}\n
+Thanks for using this bot.
+This bot can help you keep track of your attendance and maintain that sweet 75%
 
+Send /help to know how to use the bot
+`
+  ctx.reply(message)
+})
+
+// Help command
+bot.command('help', async (ctx) => {
+  const message = `Available commands:
+/help : To show the available commands
+/login username:password : To login 
+/attendance : To view attendance details
+/lastupdate : To view last attendance update details
+`
+  ctx.reply(message, {
+    reply_to_message_id: ctx.message.message_id,
+  })
+})
+
+// Login command
 bot.command('login', async (ctx) => {
   console.log('/login')
-  const credentials = ctx.message.text.split(' ')[1]
-  const username = credentials.split(':')[0]
-  const password = credentials.split(':')[1]
-  const requestBody = {
-    username: username,
-    password: password,
-  }
 
   try {
+    const credentials = ctx.message.text.split(' ')[1]
+    const username = credentials.split(':')[0]
+    const password = credentials.split(':')[1]
+    const requestBody = {
+      username: username,
+      password: password,
+    }
+
     const response = await axios.post(`${API_URL}/login`, requestBody)
     const cookie = response.data['session-cookie']
     ctx.session.session_cookie = cookie
     ctx.reply(
-      'You are logged in. Now type /attendance to get attendance details.'
+      'You are logged in. Now type /attendance to get attendance details.',
+      {
+        reply_to_message_id: ctx.message.message_id,
+      }
     )
   } catch (error) {
-    if (error.response.status == 401) {
-      ctx.reply('Invalid username or password sir')
-    } else if (error.response.status == 440) {
-      ctx.reply('Session expired. Please login again.')
+    if (error instanceof TypeError) {
+      ctx.reply('Invalid format. Please use /login username:password to log in')
+    } else if (error instanceof axios.AxiosError) {
+      if (error.response.status == 401) {
+        ctx.reply('Invalid username or password sir')
+      } else if (error.response.status == 440) {
+        ctx.reply('Session expired. Please login again.')
+      }
     }
   }
 })
 
+// Attendance command
 bot.command('attendance', async (ctx) => {
-  console.log("/attendance")
+  console.log('/attendance')
   const cookie = ctx.session.session_cookie
-  const response = await axios.get(`${API_URL}/attendance`, {
-    headers: {
-      Cookie: `session_cookie=${cookie};`,
-    },
-  })
-  response.data.subject_attendance.forEach(subject => {
-    `${subject.subject_name} (${subject.subject_code}): ${subject.present_hours}/${subject.total_hours} ${subject.present_hours}`
-  });
-  const message = `
+  if (!cookie) {
+    ctx.reply(
+      'You are logged out. Please login to view attendance details. See /help for details'
+    )
+    return
+  }
+
+  try {
+    const response = await axios.get(`${API_URL}/attendance`, {
+      headers: {
+        Cookie: `session_cookie=${cookie};`,
+      },
+    })
+
+    let attendance_data = ''
+    response.data.subject_attendance.forEach((subject) => {
+      attendance_data = attendance_data.concat(
+        `${subject.subject_code}:   ${subject.present_hours}/${subject.total_hours}   ${subject.percentage}\n`
+      )
+    })
+    /* console.log(attendance_data) */
+    const message = `
+**ATTENDANCE**
+
+\`\`\`
 Name : ${response.data.name}  
-Admission no : ${response.data.admission_no}
+
 Total attendance : ${response.data.total_attendance}
+
+Attendance details :
+
+${attendance_data}
+\`\`\`
 `
-  message.replace('.', '\\.')
-  ctx.reply(message)
+    ctx.replyWithMarkdownV2(message)
+  } catch (error) {
+    if (error instanceof axios.AxiosError) {
+      if (error.response.status == 440) {
+        ctx.reply('Session expired. Please login again.')
+      }
+    }
+  }
+})
+
+// Last update command
+bot.command('/lastupdate', async (ctx) => {
+  const cookie = ctx.session.session_cookie
+  if (!cookie) {
+    ctx.reply(
+      'You are logged out. Please login to view lastupdate details. See /help for details'
+    )
+    return
+  }
+
+  try {
+    const response = await axios.get(`${API_URL}/attendance/lastupdate`, {
+      headers: {
+        Cookie: `session_cookie=${cookie};`,
+      },
+    })
+
+    const attendanceUpdateMsg = response.data.stringify()
+    ctx.reply(attendanceUpdateMsg, {
+      reply_to_message_id: ctx.message.message_id,
+    })
+  } catch (error) {
+    if (error instanceof axios.AxiosError) {
+      if (error.response.status == 440) {
+        ctx.reply(
+          'Session expired. Please login again to view last update details.'
+        )
+      }
+    }
+  }
+})
+
+// Handle other messages
+bot.on('text', async (ctx) => {
+  const message = `<i>${Quote.getQuote().text}</i>\n
+  - <i>${Quote.getQuote().author}</i>`
+  const invalidMsg =
+    "That doesn't seem like a valid command. Use /help for available commands"
+
+  ctx
+    .replyWithHTML(message, {
+      reply_to_message_id: ctx.message.message_id,
+    })
+    .then(({message_id}) => {
+      setTimeout(() => ctx.deleteMessage(message_id), 1000)
+    })
+    .then(() => {
+      ctx.reply(invalidMsg)
+    })
 })
 
 // Start webhook via launch method (preferred)
-bot.launch({
-  webhook: {
-    // Public domain for webhook; e.g.: example.com
-    domain: SERVER_URL,
+bot
+  .launch({
+    webhook: {
+      // Public domain for webhook; e.g.: example.com
+      domain: SERVER_URL,
 
-    // Port to listen on; e.g.: 8080
-    port: PORT,
-  },
-})
+      // Port to listen on; e.g.: 8080
+      port: PORT,
+    },
+  })
+  .then(() => console.log(`🚀 Bot is up and running on port ${PORT}`))
