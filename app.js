48b4bf306a3db1f13a42557e15ea0b2c011c2820
Devadathan M B <devadathanmb@gmail.com>
2023-01-07 14:51:59 +0530

    feat: Implement lastupdate command

--- app.js
+++ app.js
@@ -150,8 +150,19 @@ bot.command('/lastupdate', async (ctx) => {
       },
     })
 
-    const attendanceUpdateMsg = response.data.stringify()
-    ctx.reply(attendanceUpdateMsg, {
+    let message = '**LASTUPDATE** \n\n```\n'
+    const lastUpdateObject = response.data
+    for (subjectCode in lastUpdateObject) {
+      message = message.concat(
+        `${utilites
+          .shortenSubjectName(lastUpdateObject[subjectCode].subject_name)
+          .padEnd(5)} : ${lastUpdateObject[subjectCode].last_update.padStart(
+          16
+        )} \n`
+      )
+    }
+    message = message.concat('```')
+    ctx.replyWithMarkdownV2(message, {
       reply_to_message_id: ctx.message.message_id,
     })
   } catch (error) {
