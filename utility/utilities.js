f577f4e1a552c3dd360e4092250a652807e0625b
Devadathan M B <devadathanmb@gmail.com>
2023-01-15 12:35:42 +0530

    style : Format code

--- utility/utilities.js
+++ utility/utilities.js
@@ -2,26 +2,26 @@
 
 // Function to get the shortened subject name
 function shortenSubjectName(subjectName) {
-  let shortenedName = ''
+  let shortenedName = "";
   subjectName
     .trim()
-    .split(' ')
+    .split(" ")
     .forEach((word) => {
-      shortenedName = shortenedName.concat(word[0])
-    })
-  return shortenedName
+      shortenedName = shortenedName.concat(word[0]);
+    });
+  return shortenedName;
 }
 
 // Function to calculate cut
 function calculateCut(presentHours, totalHours) {
-  const currentPercentage = (presentHours / totalHours) * 100
-  const totalCut = (presentHours * 100) / 75
-  const cut = totalCut - totalHours
+  const currentPercentage = (presentHours / totalHours) * 100;
+  const totalCut = (presentHours * 100) / 75;
+  const cut = totalCut - totalHours;
   if (currentPercentage > 75) {
-    return Math.floor(cut)
+    return Math.floor(cut);
   } else {
-    return Math.ceil(cut)
+    return Math.ceil(cut);
   }
 }
 
-module.exports = { shortenSubjectName, calculateCut }
+module.exports = { shortenSubjectName, calculateCut };
