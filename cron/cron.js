ebf27d75c15299318a31cc155f314916c72f511c
Devadathan M B <devadathanmb@gmail.com>
2023-01-28 15:13:32 +0530

    Add cronjob file

---
+++ cron/cron.js
@@ -0,0 +1,25 @@
+const LocalSession = require("telegraf-session-local");
+const moment = require("moment");
+const LocalSession = require("telegraf-session-local");
+const moment = require("moment");
+const localSession = new LocalSession({
+  database: "../sessions.json",
+});
+
+cron.schedule("*/10 * * * * *", () => {
+  console.log("Cron running");
+  const sessions = localSession.DB.value().sessions;
+  for (const session of sessions) {
+    console.log(session);
+    const id = session.id;
+    const createdDate = moment(session.data.created_at);
+    const now = moment();
+    const hours = parseInt(moment.duration(now.diff(createdDate)).asHours());
+
+    if (hours > 1) {
+      console.log("Removing");
+      localSession.DB.removeById(id);
+    }
+  }
+  localSession.DB.write();
+});
