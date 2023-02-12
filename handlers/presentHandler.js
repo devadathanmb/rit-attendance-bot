dd1f1cccd442eaeb3f26673416a54d45f612abcd
Devadathan M B <devadathanmb@gmail.com>
2023-02-12 13:20:30 +0530

    feat: View present data

---
+++ handlers/presentHandler.js
@@ -0,0 +1,27 @@
+// Handle present command
+const utilites = require("../utility/utilities.js");
+
+async function presentHandler(ctx) {
+  const response = ctx.response;
+
+  const presentData = response.data;
+
+  let message = "";
+  let header = `${"DATE".padEnd(16)}  ${"HOUR"}  ${"SUBJECT"}\n\n`;
+  message = message.concat(header);
+
+  presentData.forEach((presentObj) => {
+    message = message.concat(
+      `${presentObj.present_date}   ${presentObj.present_hour}     ${utilites
+        .shortenSubjectName(presentObj.subject_name)
+        .padStart(5)}\n`
+    );
+  });
+
+  ctx.reply("🥳 You were present on the following dates.");
+  const chunks = utilites.splitMsg(message);
+  for (const chunk of chunks) {
+    await ctx.reply(`\`\`\`\n${chunk}\n\`\`\``, { parse_mode: "Markdown" });
+  }
+}
+module.exports = presentHandler;
